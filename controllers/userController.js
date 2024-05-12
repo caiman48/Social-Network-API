a28a546f7e4d2fdb9a3f0fc40102d92d5f65c34c
caiman48 <caiman48clm@gmail.com>
2024-05-12 12:16:00 -0400

    created addfriend and deletefriend

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,6 +73,39 @@ const userController = {
       res.status(500).json({ message: "Failed to delete user", error });
     }
   },
+  // add friend
+  async addFriend(req, res) {
+    try {
+      const  updatedUser = await User.findByIdAndUpdate(
+        req.params.userId,
+       { $addToSet: { friends: req.params.friendId } },
+        { new: true }
+      ).populate(`friends`);
+      if (!updatedUser) {
+      return res.status(404).json({ message: "No user found with this id!" });
+      }
+      res.json(updatedUser);
+    } catch (error) {
+      res.status(500).json({ message: "Failed to add friend", error });
+    }
+  },
+  // remove friend
+  async removeFriend(req, res) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      ).populate(`friends`);   // Populating friends to show the updated list
+      if (!updatedUser) {
+        return res.status(404).json({ message: "No user found with this id!" });
+      }
+      res.json(updatedUser);
+    } catch (error) {
+      res.status(500).json({ message: "Failed to remove friend", error });
+    }
+  },
+
 };
 
 module.exports = userController;
